bd6f26bc6a21829d7d4e87f4d62c344b8148c3d4
axelbraunschweiger <axelbraunschweiger@gmail.com>
2019-03-29 20:42:32 -0500

    Empty input consecutive

--- empty_input.py
+++ empty_input.py
@@ -1,4 +1,5 @@
 
+#this method checks for empty input and stops when two times the input is empty
 def empty_input():
 	user_input = input("Enter something: ")
 	counter = 0
@@ -12,7 +13,19 @@ def empty_input():
 			if len(user_input) == 0:
 				counter += 1
 
-empty_input()
+# empty_input()
 
 
+
+#this method checks for empty input and stops when two times in a row the input is empty
+def empty_input_consec():
+	counter = 0
+	while counter != 2:
+		user_input = input("Enter something: ")
+		if len(user_input) == 0:
+			counter += 1
+		else:
+			counter = 0
+
+empty_input_consec()
 
